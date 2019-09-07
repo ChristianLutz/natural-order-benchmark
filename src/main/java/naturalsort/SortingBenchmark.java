b4e68097347503592786afe23c52c253a8eaaa6f
Christian Lutz <ChristianLutz@kreeloo.de>
2019-09-07 20:15:39 +0200

    Adding benchmark

--- src/main/java/naturalsort/SortingBenchmark.java
+++ src/main/java/naturalsort/SortingBenchmark.java
@@ -5,65 +5,259 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.jeasy.random.EasyRandom;
 import org.jeasy.random.EasyRandomParameters;
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.padler.natorder.NaturalOrderComparator;
 
+import naturalsort.berry.AlphaNumericStringComparator;
+import naturalsort.dacus.NaturalSorter;
+import naturalsort.friedrich.Strings;
+import naturalsort.koelle.AlphanumComparator;
 import net.greypanther.natsort.SimpleNaturalComparator;
 
 
 @State(Scope.Benchmark)
 public class SortingBenchmark {
 
-    private static List<String> input = getInput();
+    private static List<String> input50 = getInput(50);
+    private static List<String> input500 = getInput(500);
+    private static List<String> input5000 = getInput(5000);
+    private static List<String> input50000 = getInput(50000);
+    private static List<String> input500000 = getInput(500000);
 
     @Benchmark
-    public void smallSet() { // 50
-        Comparator<String> comparator = SimpleNaturalComparator.<String>getInstance();
-        Collections.sort(input.subList(0, 50), comparator);
+    public void padler_smallSet() { // 50
+        Comparator<String> comparator = new NaturalOrderComparator();
+        Collections.sort(new ArrayList<>(input50), comparator);
     }
 
     @Benchmark
-    public void midSet() { // 500
-        Comparator<String> comparator = SimpleNaturalComparator.<String>getInstance();
-        Collections.sort(input.subList(0, 500), comparator);
+    public void padler_midSet() { // 500
+        Comparator<String> comparator = new NaturalOrderComparator();
+        Collections.sort(new ArrayList<>(input500), comparator);
     }
 
     @Benchmark
-    public void largeSet() { // 5000
-        Comparator<String> comparator = SimpleNaturalComparator.<String>getInstance();
-        Collections.sort(input.subList(0, 5000), comparator);
+    public void padler_largeSet() { // 5000
+        Comparator<String> comparator = new NaturalOrderComparator();
+        Collections.sort(new ArrayList<>(input5000), comparator);
     }
 
     @Benchmark
-    public void xlargeSet() { // 50000
-        Comparator<String> comparator = SimpleNaturalComparator.<String>getInstance();
-        Collections.sort(input.subList(0, 50000), comparator);
+    public void padler_xlargeSet() { // 50000
+        Comparator<String> comparator = new NaturalOrderComparator();
+        Collections.sort(new ArrayList<>(input50000), comparator);
     }
 
     @Benchmark
-    public void xxlargeSet() { // 500000
-        Comparator<String> comparator = SimpleNaturalComparator.<String>getInstance();
-        Collections.sort(input.subList(0, 500000), comparator);
+    public void padler_xxlargeSet() { // 500000
+        Comparator<String> comparator = new NaturalOrderComparator();
+        Collections.sort(new ArrayList<>(input500000), comparator);
     }
 
-    private static List<String> getInput() {
-        List<String> randomStrings = new ArrayList<>(500000);
+
+    @Benchmark
+    public void dacus_smallSet() { // 50
+        Comparator<String> comparator = new NaturalSorter.NaturalComparator();
+        Collections.sort(new ArrayList<>(input50), comparator);
+    }
+
+    @Benchmark
+    public void dacus_midSet() { // 500
+        Comparator<String> comparator = new NaturalSorter.NaturalComparator();
+        Collections.sort(new ArrayList<>(input500), comparator);
+    }
+
+    @Benchmark
+    public void dacus_largeSet() { // 5000
+        Comparator<String> comparator = new NaturalSorter.NaturalComparator();
+        Collections.sort(new ArrayList<>(input5000), comparator);
+    }
+
+    @Benchmark
+    public void dacus_xlargeSet() { // 50000
+        Comparator<String> comparator = new NaturalSorter.NaturalComparator();
+        Collections.sort(new ArrayList<>(input50000), comparator);
+    }
+
+    @Benchmark
+    public void dacus_xxlargeSet() { // 500000
+        Comparator<String> comparator = new NaturalSorter.NaturalComparator();
+        Collections.sort(new ArrayList<>(input500000), comparator);
+    }
+
+
+    @Benchmark
+    public void devexed_smallSet() { // 50
+        Comparator<String> comparator = new naturalsort.devexed.NaturalOrderComparator<>();
+        Collections.sort(new ArrayList<>(input50), comparator);
+    }
+
+    @Benchmark
+    public void devexed_midSet() { // 500
+        Comparator<String> comparator = new naturalsort.devexed.NaturalOrderComparator<>();
+        Collections.sort(new ArrayList<>(input500), comparator);
+    }
+
+    @Benchmark
+    public void devexed_largeSet() { // 5000
+        Comparator<String> comparator = new naturalsort.devexed.NaturalOrderComparator<>();
+        Collections.sort(new ArrayList<>(input5000), comparator);
+    }
+
+    @Benchmark
+    public void devexed_xlargeSet() { // 50000
+        Comparator<String> comparator = new naturalsort.devexed.NaturalOrderComparator<>();
+        Collections.sort(new ArrayList<>(input50000), comparator);
+    }
+
+    @Benchmark
+    public void devexed_xxlargeSet() { // 500000
+        Comparator<String> comparator = new naturalsort.devexed.NaturalOrderComparator<>();
+        Collections.sort(new ArrayList<>(input500000), comparator);
+    }
+
+
+    @Benchmark
+    public void koelle_smallSet() { // 50
+        Comparator<String> comparator = new AlphanumComparator();
+        Collections.sort(new ArrayList<>(input50), comparator);
+    }
+
+    @Benchmark
+    public void koelle_midSet() { // 500
+        Comparator<String> comparator = new AlphanumComparator();
+        Collections.sort(new ArrayList<>(input500), comparator);
+    }
+
+    @Benchmark
+    public void koelle_largeSet() { // 5000
+        Comparator<String> comparator = new AlphanumComparator();
+        Collections.sort(new ArrayList<>(input5000), comparator);
+    }
+
+    @Benchmark
+    public void koelle_xlargeSet() { // 50000
+        Comparator<String> comparator = new AlphanumComparator();
+        Collections.sort(new ArrayList<>(input50000), comparator);
+    }
+
+    @Benchmark
+    public void koelle_xxlargeSet() { // 500000
+        Comparator<String> comparator = new AlphanumComparator();
+        Collections.sort(new ArrayList<>(input500000), comparator);
+    }
+
+
+    @Benchmark
+    public void friedrich_smallSet() { // 50
+        Comparator<String> comparator = Strings.getNaturalComparatorAscii();
+        Collections.sort(new ArrayList<>(input50), comparator);
+    }
+
+    @Benchmark
+    public void friedrich_midSet() { // 500
+        Comparator<String> comparator = Strings.getNaturalComparatorAscii();
+        Collections.sort(new ArrayList<>(input500), comparator);
+    }
+
+    @Benchmark
+    public void friedrich_largeSet() { // 5000
+        Comparator<String> comparator = Strings.getNaturalComparatorAscii();
+        Collections.sort(new ArrayList<>(input5000), comparator);
+    }
+
+    @Benchmark
+    public void friedrich_xlargeSet() { // 50000
+        Comparator<String> comparator = Strings.getNaturalComparatorAscii();
+        Collections.sort(new ArrayList<>(input50000), comparator);
+    }
+
+    @Benchmark
+    public void friedrich_xxlargeSet() { // 500000
+        Comparator<String> comparator = Strings.getNaturalComparatorAscii();
+        Collections.sort(new ArrayList<>(input500000), comparator);
+    }
+
+
+    @Benchmark
+    public void panther_smallSet() { // 50
+        Comparator<String> comparator = SimpleNaturalComparator.<String>getInstance();
+        Collections.sort(new ArrayList<>(input50), comparator);
+    }
+
+    @Benchmark
+    public void panther_midSet() { // 500
+        Comparator<String> comparator = SimpleNaturalComparator.<String>getInstance();
+        Collections.sort(new ArrayList<>(input500), comparator);
+    }
+
+    @Benchmark
+    public void panther_largeSet() { // 5000
+        Comparator<String> comparator = SimpleNaturalComparator.<String>getInstance();
+        Collections.sort(new ArrayList<>(input5000), comparator);
+    }
+
+    @Benchmark
+    public void panther_xlargeSet() { // 50000
+        Comparator<String> comparator = SimpleNaturalComparator.<String>getInstance();
+        Collections.sort(new ArrayList<>(input50000), comparator);
+    }
+
+    @Benchmark
+    public void panther_xxlargeSet() { // 500000
+        Comparator<String> comparator = SimpleNaturalComparator.<String>getInstance();
+        Collections.sort(new ArrayList<>(input500000), comparator);
+    }
+
+
+    @Benchmark
+    public void berry_smallSet() { // 50
+        Comparator<String> comparator = new AlphaNumericStringComparator();
+        Collections.sort(new ArrayList<>(input50), comparator);
+    }
+
+    @Benchmark
+    public void berry_midSet() { // 500
+        Comparator<String> comparator = new AlphaNumericStringComparator();
+        Collections.sort(new ArrayList<>(input500), comparator);
+    }
+
+    @Benchmark
+    public void berry_largeSet() { // 5000
+        Comparator<String> comparator = new AlphaNumericStringComparator();
+        Collections.sort(new ArrayList<>(input5000), comparator);
+    }
+
+    @Benchmark
+    public void berry_xlargeSet() { // 50000
+        Comparator<String> comparator = new AlphaNumericStringComparator();
+        Collections.sort(new ArrayList<>(input50000), comparator);
+    }
+
+    @Benchmark
+    public void berry_xxlargeSet() { // 500000
+        Comparator<String> comparator = new AlphaNumericStringComparator();
+        Collections.sort(new ArrayList<>(input500000), comparator);
+    }
+
+
+    private static List<String> getInput(int size) {
+        List<String> randomStrings = new ArrayList<>(size);
         EasyRandomParameters parameters = new EasyRandomParameters()
                 .seed(123L)
                 .charset(Charset.forName("UTF-8"))
                 .stringLengthRange(1, 100);
         EasyRandom easyRandom = new EasyRandom(parameters);
-        for (int i = 0; i < 500000; ++i) {
+        for (int i = 0; i < size; ++i) {
             randomStrings.add(easyRandom.nextObject(String.class));
         }
 
@@ -71,23 +265,22 @@ public class SortingBenchmark {
     }
 
     public static void main(String[] args) {
-
-
+        System.out.println("Use main");
         Options opt = new OptionsBuilder()
             .include(".*" + "Set" + ".*")
-            .timeUnit(TimeUnit.MILLISECONDS)
+//            .timeUnit(TimeUnit.MILLISECONDS)
             .forks(1)
-            .mode(Mode.SingleShotTime)
-            .measurementBatchSize(100_000_000)
-            .measurementIterations(5)
-            .warmupBatchSize(10_000_000)
-            .warmupIterations(5)
+//            .mode(Mode.Throughput)
+//            .measurementBatchSize(100_000_000)
+            .measurementIterations(3)
+//            .warmupBatchSize(10_000_000)
+            .warmupIterations(2)
             .build();
         Runner runner =  new Runner(opt);
         try {
             runner.run();
         } catch (RunnerException e) {
-                e.printStackTrace();
+            e.printStackTrace();
         }
     }
 }
